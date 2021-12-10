933f445e47bd11a6f021f0bcb77adf16da88240c
khrvr <90619079+khrvr@users.noreply.github.com>
2021-12-10 11:15:50 +0300

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -5,6 +5,61 @@
 
 using namespace std;
 
+int sign(int a) {
+	if (a == 0) {
+		return 0;
+	}
+	else if (a < 0) {
+		return -1;
+	}
+	else {
+		return 1;
+	}
+}
+
+int partition(int pivot, vector <int>& arr, vector <int>& part_arr, char c) {
+	int check = 0;
+	if (c == '<') {
+		check = 1;
+	}
+	else if (c == '>') {
+		check = -1;
+	}
+	int numberOfElements = 0;
+	for (int i = 0; i < arr.size(); ++i) {
+		if (sign(pivot - arr[i]) == check) {
+			part_arr.push_back(arr[i]);
+			++numberOfElements;
+		}
+	}
+	return numberOfElements;
+}
+
+int quickSelect(int k, vector <int> arr) {
+	int pivot = 0;
+	vector <int> part_arr;
+	int counter = -1;
+	while (arr.size() > 1) {
+		pivot = arr[rand() % arr.size()];
+		part_arr.clear();
+		counter = -1;
+		counter += partition(pivot, arr, part_arr, '<');
+		if (counter >= k) {
+			arr = move(part_arr);
+			continue;
+		}
+		counter += partition(pivot, arr, part_arr, '=');
+		if (counter >= k ) {
+			return pivot;
+		}
+		k = k - counter - 1;
+		part_arr.clear();
+		partition(pivot, arr, part_arr, '>');
+		arr = move(part_arr);
+	}
+	return arr[0];
+}
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
@@ -17,41 +72,5 @@ int main() {
 		cin >> input;
 		arr.push_back(input);
 	}
-	int pivot = 0;
-	vector <int> partition;
-	int counter = -1;
-	while (arr.size() > 1) {
-		pivot = arr[rand() % arr.size()];
-		partition.clear();
-		counter = -1;
-		for (int i = 0; i < arr.size(); ++i) {
-			if (arr[i] < pivot) {
-				partition.push_back(arr[i]);
-				++counter;
-			}
-		}
-		if (counter >= k) {
-			arr = move(partition);
-			continue;
-		}
-		for (int i = 0; i < arr.size(); ++i) {
-			if (arr[i] == pivot) {
-				partition.push_back(arr[i]);
-				++counter;
-			}
-		}
-		if (counter >= k) {
-			cout << pivot;
-			return 0;
-		}
-		k = k - counter - 1;
-		partition.clear();
-		for (int i = 0; i < arr.size(); ++i) {
-			if (arr[i] > pivot) {
-				partition.push_back(arr[i]);
-			}
-		}
-		arr = move(partition);
-	}
-	cout << arr[0];
+	cout << quickSelect(k, arr);
 }
